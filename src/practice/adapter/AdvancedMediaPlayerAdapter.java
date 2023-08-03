eaaabfe3871943a68ec6977cc7e11f7e515f403c
min <min@DESKTOP-21KKJKE>
2023-08-04 08:59:32 +0900

    adapter 업데이트

--- src/practice/adapter/AdvancedMediaPlayerAdapter.java
+++ src/practice/adapter/AdvancedMediaPlayerAdapter.java
@@ -1,16 +1,15 @@
 package practice.adapter;
 
 public class AdvancedMediaPlayerAdapter implements MediaPlayerInterface {
+	AdvancedMediaPlayerInterface advancedMediaPlayer;
 
+	public AdvancedMediaPlayerAdapter(AdvancedMediaPlayerInterface advancedMediaPlayer) {
+		this.advancedMediaPlayer = advancedMediaPlayer;
+	}
 
-    public AdvancedMediaPlayerAdapter(                    ) {
-
-    }
-
-    @Override
-    public void play(String audioType, String filename) {
-
-
-
-    }
+	@Override
+	public void play(String audioType, String filename) {
+		advancedMediaPlayer.loadFilename(filename);
+		advancedMediaPlayer.listen();
+	}
 }
