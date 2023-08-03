8d3c797a50473d5387e4def913e76e14fb0d15c9
min <min@DESKTOP-21KKJKE>
2023-08-03 13:37:46 +0900

    Decorator 실습

--- src/practice/decorator/ShapeDecorator.java
+++ src/practice/decorator/ShapeDecorator.java
@@ -1,50 +1,47 @@
 package practice.decorator;
 
-abstract class ShapeDecorator implements Shape{
-    protected Shape decoratedShape;
+abstract class ShapeDecorator implements Shape {
+	protected Shape decoratedShape;
 
-    public ShapeDecorator(Shape decoratedShape) {
-        this.decoratedShape = decoratedShape;
-    }
+	public ShapeDecorator(Shape decoratedShape) {
+		this.decoratedShape = decoratedShape;
+	}
 
-    public void draw() {
-        decoratedShape.draw();
-    }
+	public void draw() {
+		decoratedShape.draw();
+	}
 }
 
 class RedShapeDecorator extends ShapeDecorator {
 
-    public RedShapeDecorator(Shape decoratedShape) {
-        super(decoratedShape);
-    }
+	public RedShapeDecorator(Shape decoratedShape) {
+		super(decoratedShape);
+	}
 
-    @Override
-    public void draw() {
+	@Override
+	public void draw() {
+		setRedBorder(decoratedShape);
+	}
 
-
-    }
-
-    private void setRedBorder(Shape decoratedShape){
-
-
-    }
+	private void setRedBorder(Shape decoratedShape) {
+		decoratedShape.draw();
+		System.out.println("Border Color: Red");
+	}
 }
 
 class GreenShapeDecorator extends ShapeDecorator {
 
-    public GreenShapeDecorator(Shape decoratedShape) {
-        super(decoratedShape);
-    }
+	public GreenShapeDecorator(Shape decoratedShape) {
+		super(decoratedShape);
+	}
 
-    @Override
-    public void draw() {
+	@Override
+	public void draw() {
+		setGreenBorder(decoratedShape);
+	}
 
-
-
-    }
-
-    private void setRedBorder(Shape decoratedShape){
-
-        
-    }
+	private void setGreenBorder(Shape decoratedShape) {
+		decoratedShape.draw();
+		System.out.println("Border Color: Green");
+	}
 }
