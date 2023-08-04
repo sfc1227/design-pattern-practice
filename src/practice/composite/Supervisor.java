de2d159699dbd9aaf9626542c65089db070ef2c0
min <min@DESKTOP-21KKJKE>
2023-08-04 15:12:09 +0900

    composite 업데이트

--- src/practice/composite/Supervisor.java
+++ src/practice/composite/Supervisor.java
@@ -3,32 +3,32 @@ package practice.composite;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Supervisor implements Faculty{
-    private String name;
-    private String deptName;
+public class Supervisor implements Faculty {
+	private String name;
+	private String deptName;
 
-    private List<Faculty> myFacultyList;
+	private List<Faculty> myFacultyList;
 
-    Supervisor(String name, String deptName) {
-        this.name = name;
-        this.deptName = deptName;
+	Supervisor(String name, String deptName) {
+		this.name = name;
+		this.deptName = deptName;
+		this.myFacultyList = new ArrayList<>();
+	}
 
-    }
+	public void add(Faculty professor) {
+		myFacultyList.add(professor);
+	}
 
-    public void add (Faculty professor) {
+	public void remove(Faculty professor) {
+		myFacultyList.remove(professor);
+	}
 
-    }
+	public List<Faculty> getMyFaculty() {
+		return myFacultyList;
+	}
 
-    public void remove(Faculty professor) {
-
-    }
-
-    public List<Faculty> getMyFaculty() {
-        return myFacultyList;
-    }
-
-    @Override
-    public String getDetails() {
-        return (name + " is the  " + deptName);
-    }
+	@Override
+	public String getDetails() {
+		return (name + " is the  " + deptName);
+	}
 }
