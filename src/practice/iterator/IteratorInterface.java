ff8034cd1cb4cf51f01cd61d32d45a7a72685af3
min <min@DESKTOP-21KKJKE>
2023-08-04 13:04:15 +0900

    iterator 업데이트

--- src/practice/iterator/IteratorInterface.java
+++ src/practice/iterator/IteratorInterface.java
@@ -3,60 +3,63 @@ package practice.iterator;
 import java.util.LinkedList;
 
 interface IteratorInterface {
-    void first();//Reset to first element
-    String next();//get next element
-    boolean isDone();//End of collection check
-    String currentItem();//Retrieve Current Item
+	void first();// Reset to first element
+
+	String next();// get next element
+
+	boolean isDone();// End of collection check
+
+	String currentItem();// Retrieve Current Item
 }
 
 class ArtsIterator implements IteratorInterface {
-    private String[] subjects;
-    private int position;
+	private String[] subjects;
+	private int position;
 
-    public ArtsIterator(String[] subjects)  {
-        this.subjects = subjects;
-        position = 0;
-    }
+	public ArtsIterator(String[] subjects) {
+		this.subjects = subjects;
+		position = 0;
+	}
 
-    public void first()  {
-        position = 0;
-    }
+	public void first() {
+		position = 0;
+	}
 
-    public String next()  {
+	public String next() {
+		return subjects[position++];
+	}
 
-    }
+	public boolean isDone() {
+		return position == subjects.length;
+	}
 
-    public boolean isDone() {
-
-    }
-
-    public String currentItem()  {
-
-    }
+	public String currentItem() {
+		return subjects[position];
+	}
 }
 
 class ScienceIterator implements IteratorInterface {
-    private LinkedList<String> subjects;
-    private int position;
+	private LinkedList<String> subjects;
+	private int position;
 
-    public ScienceIterator(LinkedList<String> subjects)  {
-        this.subjects = subjects;
-        position = 0;
-    }
+	public ScienceIterator(LinkedList<String> subjects) {
+		this.subjects = subjects;
+		position = 0;
+	}
 
-    public void first()  {
-        position = 0;
-    }
+	public void first() {
+		position = 0;
+	}
 
-    public String next()  {
+	public String next() {
+		return subjects.get(position++);
+	}
 
-    }
+	public boolean isDone() {
+		return position == subjects.size();
+	}
 
-    public boolean isDone() {
-        
-    }
-
-    public String currentItem()  {
-        
-    }
+	public String currentItem() {
+		return subjects.get(position);
+	}
 }
--- src/practice/iterator/SubjectInterface.java
+++ src/practice/iterator/SubjectInterface.java
@@ -3,35 +3,35 @@ package practice.iterator;
 import java.util.LinkedList;
 
 public interface SubjectInterface {
-    public IteratorInterface createIterator();
+	public IteratorInterface createIterator();
 }
 
 class Arts implements SubjectInterface {
-    private String[] subjects;
+	private String[] subjects;
 
-    public Arts() {
-        subjects = new String[2];
-        subjects[0] = "Bengali";
-        subjects[1] = "English" ;
-    }
+	public Arts() {
+		subjects = new String[2];
+		subjects[0] = "Bengali";
+		subjects[1] = "English";
+	}
 
-    public IteratorInterface createIterator() {
-
-    }
+	public IteratorInterface createIterator() {
+		return new ArtsIterator(subjects);
+	}
 }
 
 class Science implements SubjectInterface {
-    private LinkedList<String> subjects;
+	private LinkedList<String> subjects;
 
-    public Science()  {
-        subjects = new LinkedList<String>();
-        subjects.addLast("Maths");
-        subjects.addLast("Comp. Sc.");
-        subjects.addLast("Physics");
-    }
+	public Science() {
+		subjects = new LinkedList<String>();
+		subjects.addLast("Maths");
+		subjects.addLast("Comp. Sc.");
+		subjects.addLast("Physics");
+	}
 
-    @Override
-    public IteratorInterface createIterator()  {
-
-    }
+	@Override
+	public IteratorInterface createIterator() {
+		return new ScienceIterator(subjects);
+	}
 }
