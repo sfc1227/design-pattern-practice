0808e11d591df75ea10f2f781131a6dbc22103fa
min <min@DESKTOP-21KKJKE>
2023-08-04 16:22:26 +0900

    Poxy 업데이트

--- src/practice/proxy/Internet.java
+++ src/practice/proxy/Internet.java
@@ -1,12 +1,12 @@
 package practice.proxy;
 
 interface Internet {
-    public void connectTo(String serverhost) throws Exception;
+	public void connectTo(String serverhost) throws Exception;
 }
 
 class RealInternet implements Internet {
-    @Override
-    public void connectTo(String serverhost) {
-
-    }
+	@Override
+	public void connectTo(String serverhost) {
+		System.out.println("Connecting to " + serverhost);
+	}
 }
--- src/practice/proxy/ProxyInternet.java
+++ src/practice/proxy/ProxyInternet.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ProxyInternet implements Internet {
+	private Internet internet = new RealInternet();
 
+	private static List<String> bannedSites;
 
-    private static List<String> bannedSites;
+	static {
+		bannedSites = new ArrayList<String>();
+		bannedSites.add("whatever.com");
+		bannedSites.add("nope.com");
+		bannedSites.add("yup.com");
+		bannedSites.add("hello.com");
+	}
 
-    static {
-        bannedSites = new ArrayList<String>();
-        bannedSites.add("whatever.com");
-        bannedSites.add("nope.com");
-        bannedSites.add("yup.com");
-        bannedSites.add("hello.com");
-    }
-
-    @Override
-    public void connectTo(String serverhost) throws Exception {
-
-    }
+	@Override
+	public void connectTo(String serverhost) throws Exception {
+		if (bannedSites.contains(serverhost.toLowerCase())) {
+			throw new Exception("Access Denied");
+		}
+		internet.connectTo(serverhost);
+	}
 }
