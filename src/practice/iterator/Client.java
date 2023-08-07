153d9798c5a95951eec25719abf7cff666f8b183
min <min@DESKTOP-21KKJKE>
2023-08-07 15:57:24 +0900

    iterator 수정

--- src/practice/iterator/Client.java
+++ src/practice/iterator/Client.java
@@ -1,29 +1,24 @@
 package practice.iterator;
 
-import javax.security.auth.Subject;
-
 public class Client {
 
-    public static void print(IteratorInterface iterator)  {
-        while (!iterator.isDone())  {
-            System.out.println(iterator.next());
-        }
-    }
+	public static void print(IteratorInterface iterator) {
+		while (!iterator.isDone()) {
+			System.out.println(iterator.next());
+		}
+	}
 
+	public static void main(String[] args) {
+		SubjectInterface Sc_subject = new Science();
+		SubjectInterface Ar_subjects = new Arts();
 
+		IteratorInterface Sc_iterator = Sc_subject.createIterator();
+		IteratorInterface Ar_iterator = Ar_subjects.createIterator();
 
-    public static void main(String[] args)
-    {
-        SubjectInterface Sc_subject = new Science();
-        SubjectInterface Ar_subjects = new Arts();
+		System.out.println("\nScience subjects :");
+		print(Sc_iterator);
 
-        IteratorInterface Sc_iterator = Sc_subject.createIterator();
-        IteratorInterface Ar_iterator = Ar_subjects.createIterator();
-
-        System.out.println("\nScience subjects :");
-        print(Sc_iterator);
-
-        System.out.println("\nArts subjects :");
-        print(Ar_iterator);
-    }
+		System.out.println("\nArts subjects :");
+		print(Ar_iterator);
+	}
 }
