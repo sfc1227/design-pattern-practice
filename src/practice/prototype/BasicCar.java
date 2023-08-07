d8f38b72daf0310587a7f876e55b9800883902bc
min <min@DESKTOP-21KKJKE>
2023-08-07 11:35:47 +0900

    Prototype 업데이트

--- src/practice/prototype/BasicCar.java
+++ src/practice/prototype/BasicCar.java
@@ -3,70 +3,60 @@ package practice.prototype;
 import java.util.Random;
 
 abstract class BasicCar implements Cloneable {
-    protected String modelName;
-    public int price;
+	protected String modelName;
+	public int price;
 
-    public String getModelName() {
-        return modelName;
-    }
+	public String getModelName() {
+		return modelName;
+	}
 
-    public void setModelName(String modelName) {
-        this.modelName = modelName;
-    }
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
 
-    public static int setPrice() {
-        int price = 0;
-        Random r = new Random();
-        int p = r.nextInt(100000);
-        price = p;
-        return price;
-    }
+	public static int setPrice() {
+		int price = 0;
+		Random r = new Random();
+		int p = r.nextInt(100000);
+		price = p;
+		return price;
+	}
 
-    public BasicCar clone() {
-        BasicCar clone = null;
-        try {
-            clone = (BasicCar)super.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        return clone;
-    }
+	public BasicCar clone() {
+		BasicCar clone = null;
+		try {
+			clone = (BasicCar) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
+	}
 }
 
 class Ford extends BasicCar implements Cloneable {
 
-    public Ford (String m)
-    {
-        modelName = m;
-    }
+	public Ford(String m) {
+		modelName = m;
+	}
 
-    @Override
-    public Ford clone()
-    {
-        Ford clone = null;
-        //???
-        
-        return clone;
-    }
+	@Override
+	public Ford clone() {
+		Ford clone = null;
+		clone = (Ford) clone();
+		return clone;
+	}
 }
 
 class Nano extends BasicCar implements Cloneable {
 
-    public Nano(String m)
-    {
-        modelName = m;
-    }
+	public Nano(String m) {
+		modelName = m;
+	}
 
-    @Override
-    public Nano clone()
-    {
-        Nano clone = null;
-        //???
-
-        return clone;
-    }
+	@Override
+	public Nano clone() {
+		Nano clone = null;
+		clone = (Nano) clone();
+		return clone;
+	}
 }
-
-
-
-
--- src/practice/prototype/BasicCarCache.java
+++ src/practice/prototype/BasicCarCache.java
@@ -3,22 +3,20 @@ package practice.prototype;
 import java.util.Hashtable;
 
 public class BasicCarCache {
-    private static Hashtable<String, BasicCar> basicCarMap = new Hashtable<String, BasicCar>();
+	private static Hashtable<String, BasicCar> basicCarMap = new Hashtable<String, BasicCar>();
 
-    public static BasicCar getCar(String carName) {
-        BasicCar cachedBasicCar = basicCarMap.get(carName);
+	public static BasicCar getCar(String carName) {
+		BasicCar cachedBasicCar = basicCarMap.get(carName);
+		return cachedBasicCar;
+	}
 
-        // ???
-        return 
-    }
+	public static void loadCache() {
+		BasicCar nano_base = new Nano("Green Nano");
+		nano_base.price = 100000;
+		basicCarMap.put("Green Nano", nano_base);
 
-    public static void loadCache() {
-        BasicCar nano_base = new Nano("Green Nano");
-        nano_base.price=100000;
-        basicCarMap.put("Green Nano", nano_base);
-
-        BasicCar ford_basic = new Ford("Ford Yellow");
-        ford_basic.price=500000;
-        basicCarMap.put("Ford Yellow", ford_basic);
-    }
+		BasicCar ford_basic = new Ford("Ford Yellow");
+		ford_basic.price = 500000;
+		basicCarMap.put("Ford Yellow", ford_basic);
+	}
 }
