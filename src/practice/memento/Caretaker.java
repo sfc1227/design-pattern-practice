43b5cbdb3cb6ea66f5f5f73ffe8c8ef8a2de8147
min <min@DESKTOP-21KKJKE>
2023-08-07 15:40:13 +0900

    Memento 업데이트

--- src/practice/memento/Caretaker.java
+++ src/practice/memento/Caretaker.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Caretaker {
-    private List<Memento> mementoList = new ArrayList<Memento>();
+	private List<Memento> mementoList = new ArrayList<Memento>();
 
-    public void add(Memento state){
-        // ???
-        
-    }
+	public void add(Memento state) {
+		// ???
+		mementoList.add(state);
+	}
 
-    public Memento get(int index){
-        return mementoList.get(index);
-    }
+	public Memento get(int index) {
+		return mementoList.get(index);
+	}
 }
--- src/practice/memento/Originator.java
+++ src/practice/memento/Originator.java
@@ -1,23 +1,23 @@
 package practice.memento;
 
 public class Originator {
-    private String state;
+	private String state;
 
-    public void setState(String state){
-        this.state = state;
-    }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-    public String getState(){
-        return state;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public Memento saveStateToMemento(){
-        // ???
-        
-    }
+	public Memento saveStateToMemento() {
+		// ???
+		return new Memento(state);
+	}
 
-    public void getStateFromMemento(Memento memento){
-        state = memento.getState();
-    }
+	public void getStateFromMemento(Memento memento) {
+		state = memento.getState();
+	}
 
 }
