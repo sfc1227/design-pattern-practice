101aef8e486b28df43291f53f01b9443ad63f566
min <min@DESKTOP-21KKJKE>
2023-08-07 14:49:38 +0900

    Flyweight 업데이트

--- src/practice/flyweight/CounterStrike.java
+++ src/practice/flyweight/CounterStrike.java
@@ -3,46 +3,49 @@ package practice.flyweight;
 import java.util.Random;
 
 public class CounterStrike {
-    // All player types and weapons (used by getRandPlayerType()
-    // and getRandWeapon()
+	// All player types and weapons (used by getRandPlayerType()
+	// and getRandWeapon()
 
-    // Utility methods to get a random player type and weapon
-    public static String getRandPlayerType()   {
-        Random r = new Random();
+	// Utility methods to get a random player type and weapon
+	public static String getRandPlayerType() {
+		Random r = new Random();
 
-        // Will return an integer between [0,2)
-        int randInt = r.nextInt(playerType.length);
+		// Will return an integer between [0,2)
+		int randInt = r.nextInt(playerType.length);
 
-        // return the player stored at index 'randInt'
-        return playerType[randInt];
-    }
+		// return the player stored at index 'randInt'
+		return playerType[randInt];
+	}
 
-    public static String getRandWeapon()   {
-        Random r = new Random();
+	public static String getRandWeapon() {
+		Random r = new Random();
 
-        // Will return an integer between [0,5)
-        int randInt = r.nextInt(weapons.length);
+		// Will return an integer between [0,5)
+		int randInt = r.nextInt(weapons.length);
 
-        // Return the weapon stored at index 'randInt'
-        return weapons[randInt];
-    }
+		// Return the weapon stored at index 'randInt'
+		return weapons[randInt];
+	}
 
-    private static String[] playerType =  {"Terrorist", "CounterTerrorist"};
+	private static String[] playerType = { "Terrorist", "CounterTerrorist" };
 
-    private static String[] weapons = {"AK-47", "Maverick", "Gut Knife", "Desert Eagle"};
+	private static String[] weapons = { "AK-47", "Maverick", "Gut Knife", "Desert Eagle" };
 
-    public static void main(String args[])
-    {
-        /* Assume that we have a total of 10 players in the game. */
-        for (int i = 0; i < 10; i++)  {
-            /* getPlayer() is called simply using the class name since the method is a static one */
-            // ???
+	public static void main(String args[]) {
+		/* Assume that we have a total of 10 players in the game. */
+		for (int i = 0; i < 10; i++) {
+			/*
+			 * getPlayer() is called simply using the class name since the method is a
+			 * static one
+			 */
+			// ???
+			Player p = PlayerFactory.getPlayer(getRandPlayerType());
 
-            /* Assign a weapon chosen randomly uniformly from the weapon array  */
-            p.assignWeapon(getRandWeapon());
+			/* Assign a weapon chosen randomly uniformly from the weapon array */
+			p.assignWeapon(getRandWeapon());
 
-            // Send this player on a mission
-            p.mission();
-        }
-    }
+			// Send this player on a mission
+			p.mission();
+		}
+	}
 }
--- src/practice/flyweight/PlayerFactory.java
+++ src/practice/flyweight/PlayerFactory.java
@@ -3,31 +3,29 @@ package practice.flyweight;
 import java.util.HashMap;
 
 public class PlayerFactory {
-    private static HashMap<String, Player> hm = new HashMap<String, Player>();
+	private static HashMap<String, Player> hm = new HashMap<String, Player>();
 
-    public static Player getPlayer(String type) {
-        Player p = null;
+	public static Player getPlayer(String type) {
+		Player p = null;
 
-        if (hm.containsKey(type)) {
-            // ???
-        
-        }
-        else {
-            switch (type) {
-                case "Terrorist":
-                    System.out.println("Terrorist Created");
-                    p = new Terrorist();
-                    break;
-                case "CounterTerrorist":
-                    System.out.println("Counter Terrorist Created");
-                    p = new CounterTerrorist();
-                    break;
-                default:
-                    System.out.println("Unreachable code!");
-            }
+		if (hm.containsKey(type)) {
+			return hm.get(type);
+		} else {
+			switch (type) {
+			case "Terrorist":
+				System.out.println("Terrorist Created");
+				p = new Terrorist();
+				break;
+			case "CounterTerrorist":
+				System.out.println("Counter Terrorist Created");
+				p = new CounterTerrorist();
+				break;
+			default:
+				System.out.println("Unreachable code!");
+			}
 
-            hm.put(type, p);
-        }
-        return p;
-    }
+			hm.put(type, p);
+		}
+		return p;
+	}
 }
